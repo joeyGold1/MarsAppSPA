796f4fdbb53dc4e46ba39ed3396c6ac3bdbdfb7f
Joey Gold <joey.gold@softwire.com>
2021-07-15 15:58:40 +0100

    Added left and right buttons to scroll through photos.

--- mars-app-spa/src/App.tsx
+++ mars-app-spa/src/App.tsx
@@ -36,3 +36,7 @@ function App() {
 }
 
 export default App;
+
+
+
+<div>Icons made by <a href="https://www.flaticon.com/authors/catalin-fertu" title="Catalin Fertu">Catalin Fertu</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- mars-app-spa/src/FCs/PhotoCollectionFC.tsx
+++ mars-app-spa/src/FCs/PhotoCollectionFC.tsx
@@ -7,9 +7,33 @@ import { PhotoFC } from './PhotoFC'
 export const PhotoCollection:React.FC<{photos:PhotoI[]}> = ({photos})=>{
     const [pagination,setPagination] = useState({start:0,end:numPerPage})
     const photoList = photos.map((photo: PhotoI)=>{return (<PhotoFC photo={photo}/>)}).slice(pagination.start,pagination.end);
+    const leftButton = (<button><img src="./Images/NASAlogo.png" alt="<" 
+        onClick={()=>{
+            const newStart = Math.max((pagination.start - numPerPage),0);
+            const newEnd = newStart+numPerPage;
+            setPagination( {start:newStart,
+                            end:  newEnd    }
+            );
+        }
+    } /></button>);
+    
+    const rightButton = (<button><img src="./Images/NASAlogo.png" alt=">" 
+    onClick={()=>{
+        const newStart = Math.min((pagination.start + numPerPage),photos.length-1);
+        const newEnd = newStart+numPerPage;
+        setPagination( {start:newStart,
+                        end:  newEnd    }
+        );
+    }
+    } /></button>);
+    console.log("Pagination start: " + pagination.start);
+    console.log("Pagination end: "+ pagination.end);
     return (
         <>
+                {leftButton}{rightButton}
             <div>
+
+
                 {photoList}
             </div>
         </>
