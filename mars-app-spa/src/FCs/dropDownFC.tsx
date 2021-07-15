5c5111874c2ff205d37b9ede9f87a1c8829f2035
Harvey Yau <Harvey.Yau@softwire.com>
2021-07-15 10:33:01 +0100

    add CSS to make dropdowns readable

--- mars-app-spa/src/FCs/dropDownFC.tsx
+++ mars-app-spa/src/FCs/dropDownFC.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Select from "react-select";
+import "./dropDownFC.css";
 
 export const DropDownFC: React.FC<{
   options: { value: any; label: string }[];
   onSelect: (x: any) => void;
 }> = ({ options, onSelect }) => {
   return (
-    <Select
+    <Select className="dropDownSelect"
       options={options}
       onChange={(selectedOption) =>
         onSelect(selectedOption ? selectedOption.value : "")
