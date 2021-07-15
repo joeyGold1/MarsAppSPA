88a58d0213647e46daf157eee8c1f42f899e9155
Harvey Yau <Harvey.Yau@softwire.com>
2021-07-15 13:47:07 +0100

    Add title, change font color of sol box

--- mars-app-spa/src/App.tsx
+++ mars-app-spa/src/App.tsx
@@ -24,6 +24,7 @@ function App() {
     >
       <div className="App">
         <header className="App-header">
+          <h1>Mars Rover Photo App</h1>
           <RoverDropDownFC />
           <CameraDropDownFC />
           <SolInputFC />
